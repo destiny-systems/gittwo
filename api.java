5b44fd45420392f957e30551e45987b8063cbe09
Destiny Systems <destinysystems00@gmail.com>
2025-05-13 21:37:38 +0300

    updated api in java script

--- api.java
+++ api.java
@@ -9,3 +9,41 @@ public class MainApplication {
         SpringApplication.run(MainApplication.class, args);
     }
 }
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, world!";
+    }
+
+    @PostMapping("/greet")
+    public String greet(@RequestBody User user) {
+        return "Hello, " + user.getName() + "!";
+    }
+}
+package com.example.demo;
+
+public class User {
+    private String name;
+
+    // Constructors
+    public User() {}
+    public User(String name) {
+        this.name = name;
+    }
+
+    // Getter and Setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
